4f6cc2bb86a06733c331b6131d1a785fc658b7b6
SGabriel333 <30908425+SGabriel333@users.noreply.github.com>
2017-10-08 03:55:04 -0300

    Update 20171005184539_create_data_users.rb
    apeelido por apellido

--- db/migrate/20171005184539_create_data_users.rb
+++ db/migrate/20171005184539_create_data_users.rb
@@ -2,7 +2,7 @@ class CreateDataUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :data_users do |t|
       t.string :nombre
-      t.string :apeelido
+      t.string :apellido
       t.string :telefono
       t.string :direccion
       t.string :correo
