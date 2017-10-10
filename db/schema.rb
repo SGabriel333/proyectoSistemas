cc2602369e5c76421365fd0fbebd90c52e13fa13
Sergio Gabriel <sergio.gabriel.el10@gmail.com>
2017-10-10 19:13:42 -0300

    role.rb

---
+++ db/schema.rb
@@ -0,0 +1,96 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+
+ActiveRecord::Schema.define(version: 20171008223307) do
+
+ActiveRecord::Schema.define(version: 20171008184208) do
+
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "alumnos", force: :cascade do |t|
+    t.integer "cant_horas"
+    t.bigint "carrera_id"
+    t.bigint "datos_personal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["carrera_id"], name: "index_alumnos_on_carrera_id"
+    t.index ["datos_personal_id"], name: "index_alumnos_on_datos_personal_id"
+  end
+
+  create_table "carreras", force: :cascade do |t|
+    t.string "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+
+  create_table "data_users", force: :cascade do |t|
+    t.string "nombre"
+    t.string "apeelido"
+    t.string "telefono"
+    t.string "direccion"
+    t.string "correo"
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_data_users_on_users_id"
+  end
+
+
+  create_table "datos_personals", force: :cascade do |t|
+    t.string "nombre"
+    t.string "apellido"
+    t.string "telefono"
+    t.string "direccion"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_datos_personals_on_user_id"
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.string "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+
+    t.string "name"
+    t.string "last_name"
+
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "alumnos", "carreras"
+  add_foreign_key "alumnos", "datos_personals"
+
+  add_foreign_key "data_users", "users", column: "users_id"
+
+  add_foreign_key "datos_personals", "users"
+end
