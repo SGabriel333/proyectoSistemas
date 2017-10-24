6016db2f66ce11ddeba96281ccb0cebd52c31029
Sergio Gabriel <sergio.gabriel.el10@gmail.com>
2017-10-24 18:49:22 -0300

    CRUD USUARIO
    Usuarios con Roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :alumnos
   resources :carreras
-  resources :datos_personals
+ 
   resources :user
-  get 'home/index'
+  resources :role
 
-  root to: 'home#index'
+  
   devise_for :users
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
