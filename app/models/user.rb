2daa553820e8ee8a4d41106b396aa59ec319c1ae
Sergio Gabriel <sergio.gabriel.el10@gmail.com>
2017-11-30 00:20:44 -0300

    Revert "Merge branch 'master' of https://github.com/SGabriel333/proyectoSistemas"
    Auditoria y Usuarios with modal
    Vistas segun roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,30 +2,14 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,:registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+  def role
+  end
 
-  audited
- 
- def current_user
-  User.find_by_username 'root'
-end
-
- def is_admin?
-    self.has_role?(:admin)
- end
-
- def is_adminExt?
-    self.has_role?(:adminExt)
- end
-
- def is_adminInv?
-    self.has_role?(:adminInv)
- end
-
- def is_investigador?
-    self.has_role?(:Investigador)
- end
+  def is_admin?
+    self.role == "admin"
+  end
 
 
 end
